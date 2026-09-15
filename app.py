33d8f525457daa094a72eca8a83f5ae4a8893639
MirazizK2012 <bgz4nwkrhh@privaterelay.appleid.com>
2026-09-15 07:25:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-print("test")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+def rectangle():
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+rectangle()
+
+t.left(180)
+t.forward(300)
+t.left(180)
+
+def triangle():
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+    t.forward(90)
+    t.left(120)
+triangle()
