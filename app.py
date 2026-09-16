28335319dcdeeeb2062f8c95dbfdda42b980294a
MirazizK2012 <bgz4nwkrhh@privaterelay.appleid.com>
2026-09-16 09:23:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+def right():
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(125)
+    t.left(90)
+right()
+
+turtle.done()
