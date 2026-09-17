911e0624c33372e2c206df6e3dc20534a851c6e8
MirazizK2012 <bgz4nwkrhh@privaterelay.appleid.com>
2026-09-17 10:24:33 -0400

    y
    y

---
+++ assesment2.py
@@ -0,0 +1,15 @@
+import turtle
+from turtle import *
+t = Turtle()
+def star(x,y):
+    for i in range(5):
+        t.forward(x)
+        t.right(y)
+def starshell(iRange):
+    length=5
+    rotate=144
+    for i in range(iRange):
+        star(length,rotate)
+        length +=5
+        t.right(5)
+starshell(60)
